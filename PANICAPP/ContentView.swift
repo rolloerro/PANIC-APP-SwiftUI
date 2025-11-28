403bd0ed822430d369e75ca54055843bf2d47d61
Vladimir Kopylov <pharmdoktor1972@icloud.com>
2025-11-28 22:31:10 +0300

    Initial Commit

---
+++ PANICAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PANICAPP
+//
+//  Created by Vladimir Kopylov on 28/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PANICAPP/PANICAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PANICAPPApp.swift
+//  PANICAPP
+//
+//  Created by Vladimir Kopylov on 28/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct PANICAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
